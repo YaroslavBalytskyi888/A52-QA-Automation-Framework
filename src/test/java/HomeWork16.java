5e829fce39fa7465fd90af001c76d6505a0bd49f
YaroslavBalytskyi888 <balitskyi8@gmail.com>
2024-01-29 18:46:18 -0600

    Create HomeWork16.java

---
+++ src/test/java/HomeWork16.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeWork16 extends BaseTest {
+    @Test
+    public void registrationNavigationTest() {
+        WebElement registrationLink = driver.findElement(By.ByCssSelector("[href='registration']" ));
+        registrationLink.click();
+        WebElement registrationText = driver.findElement(By.xpath("//h2[contains(text(), 'Register new account or')]"));
+        Assert.assertTrue(registrationText.isDisplayed());
+        closeBrowser();
+    }
+    }
+}
